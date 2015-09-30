301108bc09b3c3ac8eadecdd1f51c132d9a855c2
sankar <sankar.suda@gmail.com>
2015-09-30 13:33:26 +0530

    headers change

--- src/FormServiceProvider.php
+++ src/FormServiceProvider.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the Speedwork framework.
+ *
+ * @link http://github.com/speedwork
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Speedwork\Provider;
 
 use Speedwork\Container\Container;
--- src/SpeedworkServiceProvider.php
+++ src/SpeedworkServiceProvider.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This file is part of the Speedwork package.
+ * This file is part of the Speedwork framework.
  *
- * (c) 2s Technologies <info@2stechno.com>
+ * @link http://github.com/speedwork
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
